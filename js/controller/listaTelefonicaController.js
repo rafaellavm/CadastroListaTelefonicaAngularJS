9d383493d61123ad9b4306a15526c39cae84cb35
Rafaela <rafaela.vm@gmail.com>
2017-01-19 19:06:09 -0200

    3. Usando diretivas. Uso do ngClass, ngOptions, ngModel

--- js/controller/listaTelefonicaController.js
+++ js/controller/listaTelefonicaController.js
@@ -18,8 +18,39 @@ angular.module('listaTelefonica')
         }
     ];
 
+    $scope.operadoras = [
+        {
+            nome: 'Oi',
+            codigo: 14, 
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Tim',
+            codigo: 15, 
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Vivo',
+            codigo: 41, 
+            categoria: 'Celular'
+        },
+        {
+            nome: 'GVT',
+            codigo: 25, 
+            categoria: 'Fixo'
+        },
+        {
+            nome: 'Embratel',
+            codigo: 21, 
+            categoria: 'Fixo'
+        }
+    ];
+
+    $scope.classe1 = "selecionado";
+    $scope.classe2 = "negrito";
+
     $scope.adicionarContato = function(contato){
         $scope.contatos.push(angular.copy(contato));
-        //delete $scope.contato;
+        delete $scope.contato;
     };
 });
